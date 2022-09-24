3c765c984f070fbbaa0e8c7f5e64eb666fecbb01
Alex Cheung <alexc.sfu@gmail.com>
2022-09-24 01:29:57 -0700

    added action and reducer

--- index.js
+++ index.js
@@ -17,10 +17,37 @@
  * @Second --> The only way to change the state is to dispatch an action (a function), an object describing what happened. You are not allowed to directly update the state object.
  * e.g. @action = {type: buy_cake, payload: num}
  *
- * @Third --> To specify how the state tree is transformed by actions, you write pure reducers (pure functions).
- * e.g. @reducer = (prevState, action) => newState
+ * @Third --> To specify how the state tree is transformed by actions, you write pure reducers (pure functions). It accepts state and action as arguments, and returns the next state of the application.
+ * e.g. @reducer = (state, action) => newState
  * e.g.
  * const reducer = (state, action) => {
  *    switch (action.type) {
  *      case buy_cake: return {number_of_cakes: state.number_of_cakes - 1}}}
  */
+const buy_cake = "buy_cake";
+
+const action = {
+  type: buy_cake,
+  info: "first redux action",
+};
+
+// action creator (a function that returns an action)
+const buyCake = () => {
+  return action;
+};
+
+// state
+const initialState = {
+  numOfCakes: 10,
+};
+
+// reducer
+// it is important to know that for any action, we are return a new state object instead of mutating the original state object
+const buyCakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case buy_cake:
+      return { ...state, numOfCakes: state.numOfCakes - 1 }; //make a new copy of the state first, then return the new state
+    default:
+      return state;
+  }
+};
